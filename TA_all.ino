7d971c57e5dcc981b047ddc477ea2533c4e35c36
fauzanbakri <mfzn.zoldyck@gmail.com>
2020-07-15 01:59:38 +0800

    done

--- TA_all.ino
+++ TA_all.ino
@@ -1,19 +1,67 @@
 #include "painlessMesh.h"
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <WiFiClient.h>
+#include <string.h>
 #include <Wire.h>
-#include <RtcDS3231.h>
 #include <SD.h>
 #include <SPI.h>
-#define   MESH_PREFIX     "whateverYouLike"
-#define   MESH_PASSWORD   "somethingSneaky"
-#define   MESH_PORT       5555
+#include <MQ7.h>
+#include <DHT.h>
+#include <MQ135.h>
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+
+#define MESH_PREFIX "AirQualityMonitoring"
+#define MESH_PASSWORD "12345678"
+#define MESH_PORT 5555
 #define countof(a) (sizeof(a) / sizeof(a[0]))
 
-RtcDS3231<TwoWire> Rtc(Wire);
-
+int dhtpin = 0;
+TinyGPSPlus gps;
+DHT dht(dhtpin, DHT22);
+SoftwareSerial ss(5, 2);
 Scheduler userScheduler;
+int pin_pm25 = 4;
+int mux = 3;
+
+byte buff[2];
+//unsigned long duration1;
+//unsigned long starttime1;
+//unsigned long endtime1;
+unsigned long duration2;
+unsigned long starttime2;
+unsigned long endtime2;
+unsigned long sampletime_ms = 1000;
+//unsigned long lowpulseoccupancy1 = 0;
+unsigned long lowpulseoccupancy2 = 0;
+float temperature, hum;
+//float ratio1 = 0;
+//float concentration1 = 0;
+float ratio2 = 0;
+float concentration2 = 0;
 painlessMesh  mesh;
+const char* ssid = "fznn";
+const char* password = "fauzan97";
+const char* mqtt_server = "192.168.43.170";
+const char* mqtt_username = "fauzan";
+const char* mqtt_password = "fauzan97";
+
+WiFiClient wifiClient;
+PubSubClient client(mqtt_server, 1883, wifiClient); 
+
+MQ135 gasSensor = MQ135(A0);
+MQ7 mq7(A0,5.0);
+int interval_start;
+int interval_start_wr;
+float rzero = gasSensor.getRZero();
+int ppm, ppm_co;
 int timetemp, date_temp, month_temp, year_temp;
 File myFile, myFile1, myFile2, myFile3, myFile4, myFile5, myFile6;
+float latitude , longitude;
+int year , month , date, hour , minute , second;
+String date_str , time_str , lat_str , lng_str;
+int date_inc;
 
 void sendMessage() ;
 
@@ -58,7 +106,6 @@ void receivedCallback( uint32_t from, String &msg ) {
   int count = 0;
   char char_append;
   String str = msg;
-  RtcDateTime now = Rtc.GetDateTime();
   String id_self = String(mesh.getNodeId());
   String id = String(from);
   String row, row_item, check, id_check, data_from;
@@ -79,32 +126,25 @@ void receivedCallback( uint32_t from, String &msg ) {
     Serial.println("error opening from.csv");
   }
   myFile3.close();
-//    Serial.print("check = ");
-//    Serial.print(check);
-//    Serial.print("\n");
-//    Serial.print("id = ");
-//    Serial.print(id);
-//    Serial.print("\n");  
   if  (String(check)==id) {
-    int timelast = timetemp+120;
-    int timenow = (hour(now).toInt() * 3600) + (minute(now).toInt() * 60) + second(now).toInt();
-    int date_now = date(now).toInt();
-    int month_now = month(now).toInt();
-    int year_now = year(now).toInt();
+    int timelast = timetemp+60000;
+    int timenow = millis();
+//    int date_now = date;
+//    int month_now = month;
+//    int year_now = year;
     Serial.print("timelast = ");
     Serial.print(timelast);
     Serial.print("\n");
     Serial.print("timenow = ");
     Serial.print(timenow);
     Serial.print("\n");
-    if  (timenow > timelast || date_now > date_temp || month_now > month_temp || year_now > year_temp){
+    if  (timenow > timelast){
       SD.remove("from.csv");
-      timetemp = (hour(now).toInt() * 3600) + (minute(now).toInt() * 60) + second(now).toInt();
+      timetemp = millis();
     }else{
-      Serial.print("wait 30 second to accept from same node\n");
+      Serial.print("wait 1 minute to accept from same node\n");
     }
   }else{
-//    SD.remove("from.csv");
   myFile2 = SD.open("from.csv", FILE_WRITE);
   if (myFile2) {
     myFile2.print(from);
@@ -112,22 +152,16 @@ void receivedCallback( uint32_t from, String &msg ) {
     Serial.println("error write to from.csv");
   }
   myFile2.close();
-//  Serial.print("check2 = ");
-//  Serial.print(check);
-//  Serial.print("\n");
-//  Serial.print("id2 = ");
-//  Serial.print(id);
-//  Serial.print("\n");
-  timetemp = (hour(now).toInt() * 3600) + (minute(now).toInt() * 60) + second(now).toInt();
-  date_temp = date(now).toInt();
-  month_temp = month(now).toInt();
-  year_temp = year(now).toInt();
+  timetemp = millis();
+//  date_temp = date;
+//  month_temp = month;
+//  year_temp = year;
   Serial.print("Timetemp = ");
   Serial.print(timetemp);
   Serial.print("\n");
   while (count < c) {
     row = getValue(msg, '#', count);
-    row_item = getValue(row, ',', 2);
+    row_item = getValue(row, ',', 9);
     id_check = String(row_item);
     if (id_check == id_self) {
       Serial.print("can't write this row to csv because this data from here = ");
@@ -136,8 +170,8 @@ void receivedCallback( uint32_t from, String &msg ) {
     else {
       myFile4 = SD.open("rcvdata.csv", FILE_WRITE);
       if (myFile4) {
-        myFile4.print(row);
-        myFile4.println("#");
+        myFile4.println(row + "#");
+//        myFile4.println("#");
         
       }else{
       Serial.println("Failed write rcvdata.csv");  
@@ -161,70 +195,44 @@ void changedConnectionCallback() {
 void nodeTimeAdjustedCallback(int32_t offset) {
   Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(), offset);
 }
-
-void setup() {
-  Serial.begin(9600);
-  Serial.print("compiled: ");
-  Serial.print(__DATE__);
-  Serial.println(__TIME__);
-  Wire.begin(5, 4);
-  Rtc.Begin();
-  RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
-  printDate(compiled);
-  printTime(compiled);
-  hour(compiled);
-  minute(compiled);
-  second(compiled);
-  year(compiled);
-  month(compiled);
-  date(compiled);
+void setup_wifi() {
   Serial.println();
-  if (!Rtc.IsDateTimeValid())
-  {
-    if (Rtc.LastError() != 0)
-    {
-      Serial.print("RTC communications error = ");
-      Serial.println(Rtc.LastError());
-    }
-    else
-    {
-      Serial.println("RTC lost confidence in the DateTime!");
-      Rtc.SetDateTime(compiled);
-    }
-  }
-  if (!Rtc.GetIsRunning())
-  {
-    Serial.println("RTC was not actively running, starting now");
-    Rtc.SetIsRunning(true);
-  }
-  RtcDateTime now = Rtc.GetDateTime();
-  if (now < compiled)
-  {
-    Serial.println("RTC is older than compile time!  (Updating DateTime)");
-    Rtc.SetDateTime(compiled);
-  }
-  else if (now > compiled)
-  {
-    Serial.println("RTC is newer than compile time. (this is expected)");
-  }
-  else if (now == compiled)
-  {
-    Serial.println("RTC is the same as compile time! (not expected but all is fine)");
-  }
-  Rtc.Enable32kHzPin(false);
-  Rtc.SetSquareWavePin(DS3231SquareWavePin_ModeNone);
-
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  delay(5000);
+}
+int a;
+void setup() {
+  Serial.begin(115200);
+  pinMode(3, FUNCTION_3);
+  pinMode(mux, OUTPUT);
+  pinMode(dhtpin, INPUT);
+  dht.begin();
+  ss.begin(9600);
+//  pinMode(pin_pm10,INPUT);
+//  starttime1 = millis();
+  pinMode(pin_pm25,INPUT);
+  starttime2 = millis();
+  interval_start = millis();
+  interval_start_wr = millis();
+  
   Serial.print("Initializing SD card...");
   if (!SD.begin(15)) {
     Serial.println("initialization failed!");
     return;
   }
   Serial.println("initialization done.");
-
-  //mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
+  pinMode(16, INPUT);
+  if (digitalRead(16) == 1){
+   setup_wifi(); 
+   a = 1;
+  }else{
+   a = 0;
+  //  mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
   //  mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
 
-  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT);
   mesh.onReceive(&receivedCallback);
   mesh.onNewConnection(&newConnectionCallback);
   mesh.onChangedConnections(&changedConnectionCallback);
@@ -232,39 +240,76 @@ void setup() {
 
   userScheduler.addTask( taskSendMessage );
   taskSendMessage.enable();
+  }
 }
 
 void loop() {
-
-  if (!Rtc.IsDateTimeValid())
-  {
-    if (Rtc.LastError() != 0)
-    {
-      Serial.print("RTC communications error = ");
-      Serial.println(Rtc.LastError());
-    }
-    else
-    {
-      Serial.println("RTC lost confidence in the DateTime!");
-    }
+  Serial.println(a);
+  if (a == 0){
+    mesh.update();
+  } else {
+    mqtt();
   }
-  RtcDateTime now = Rtc.GetDateTime();
-  int interval = minute(now).toInt();
-  int interval2 = second(now).toInt();
-  if (interval2 == 0) {
+  if (digitalRead(16) != a){
+    ESP.restart();
+  }
+  gps_read2();
+  dht22();
+  mq135_mq7();
+  int interval_end = millis();
+  int interval_end_wr = millis();
+  if (interval_end - interval_start > 1000) {
+  Serial.print("Date: ");
+  Serial.print(date_str);
+  Serial.print(", Time: ");
+  Serial.print(time_str);
+  Serial.print(", Temperature: ");
+  Serial.print(temperature);
+  Serial.print(", Humidity: ");
+  Serial.print(hum);
+  Serial.print(", CO: ");
+  Serial.print(ppm_co);
+  Serial.print(", CO2: ");
+  Serial.print(ppm);
+  Serial.print(", PM2.5: ");
+  Serial.print(concentration2);
+  Serial.print(", ID: ");
+  Serial.print(mesh.getNodeId());
+  Serial.print(", Latitude: ");
+  Serial.print(latitude);
+  Serial.print(", Longitude: ");
+  Serial.println(longitude);
+  dsm501();
+  if (interval_end_wr - interval_start_wr > 60000){
     SD.remove("datasend.csv");
+    Serial.print("Write");
     writeToCsv();
+    interval_start_wr = millis();
   }
-  delay(1000);
-  mesh.update();
+    interval_start = millis();
+  }
+  
 }
 
 void writeToCsv() {
+  String timestamp = time_str;
+  String datestamp = date_str;
   myFile5 = SD.open("tempdata.csv", FILE_WRITE);
   if (myFile5) {
-    RtcDateTime now = Rtc.GetDateTime();
-    String timestamp = printTime(now);
-    String datestamp = printDate(now);
+    myFile5.print(ppm);
+    myFile5.print(",");
+    myFile5.print(ppm_co);
+    myFile5.print(",");
+    myFile5.print(temperature);
+    myFile5.print(",");
+    myFile5.print(hum);
+    myFile5.print(",");
+    myFile5.print(concentration2);
+    myFile5.print(",");
+    myFile5.print(latitude);
+    myFile5.print(",");
+    myFile5.print(longitude);
+    myFile5.print(",");
     myFile5.print(datestamp);
     myFile5.print(",");
     myFile5.print(timestamp);
@@ -280,9 +325,20 @@ void writeToCsv() {
 
   myFile6 = SD.open("datasend.csv", FILE_WRITE);
   if (myFile6) {
-    RtcDateTime now = Rtc.GetDateTime();
-    String timestamp = printTime(now);
-    String datestamp = printDate(now);
+    myFile6.print(ppm);
+    myFile6.print(",");
+    myFile6.print(ppm_co);
+    myFile6.print(",");
+    myFile6.print(temperature);
+    myFile6.print(",");
+    myFile6.print(hum);
+    myFile6.print(",");
+    myFile6.print(concentration2);
+    myFile6.print(",");
+    myFile6.print(lat_str);
+    myFile6.print(",");
+    myFile6.print(lng_str);
+    myFile6.print(",");
     myFile6.print(datestamp);
     myFile6.print(",");
     myFile6.print(timestamp);
@@ -297,74 +353,179 @@ void writeToCsv() {
   myFile6.close();
 }
 
-String printDate(const RtcDateTime& dt)
-{
-  char datestring[20];
-  sprintf(datestring, "%02d/%02d/%d",
-          dt.Month(),
-          dt.Day(),
-          dt.Year() );
-  Serial.println(datestring);
-  return datestring;
+void mq135_mq7() { 
+    digitalWrite(mux, LOW);
+    ppm = gasSensor.getPPM();
+    digitalWrite(mux, HIGH);
+    ppm_co = mq7.getPPM();
 }
-String printTime(const RtcDateTime& dt)
+void gps_read2()
 {
-  char timestring[20];
-  sprintf(timestring, "%02d:%02d",
-          dt.Hour(),
-          dt.Minute() );
-  Serial.println(timestring);
-  return timestring;
-}
-String hour(const RtcDateTime& dt)
-{
-  char timestring[20];
-  sprintf(timestring, "%d",
-          dt.Hour() );
-  //    Serial.println(timestring);
-  return timestring;
-}
-String minute(const RtcDateTime& dt)
-{
-  char timestring[20];
-  sprintf(timestring, "%d",
-          dt.Minute() );
-  //    Serial.println(timestring);
-  return timestring;
-}
 
-String second(const RtcDateTime& dt)
-{
-  char timestring[20];
-  sprintf(timestring, "%d",
-          dt.Second() );
-  //    Serial.println(timestring);
-  return timestring;
-}
-String year(const RtcDateTime& dt)
-{
-  char timestring[20];
-  sprintf(timestring, "%d",
-          dt.Year() );
-  //    Serial.println(timestring);
-  return timestring;
-}
-String month(const RtcDateTime& dt)
-{
-  char timestring[20];
-  sprintf(timestring, "%d",
-          dt.Month() );
-  //    Serial.println(timestring);
-  return timestring;
-}
-String date(const RtcDateTime& dt)
-{
-  char timestring[20];
-  sprintf(timestring, "%d",
-          dt.Day() );
-  //    Serial.println(timestring);
-  return timestring;
-}
+  while (ss.available() > 0)
+    if (gps.encode(ss.read()))
+    {
+      if (gps.location.isValid())
+      {
+        latitude = gps.location.lat();
+        lat_str = String(latitude , 6);
+        longitude = gps.location.lng();
+        lng_str = String(longitude , 6);
+      }
 
+      if (gps.date.isValid())
+      {
+        date_str = "";
+        date = gps.date.day() + date_inc;
+        month = gps.date.month();
+        year = gps.date.year();
 
+        if (date < 10)
+          date_str = '0';
+        date_str += String(date);
 
+        date_str += "/";
+
+        if (month < 10)
+          date_str += '0';
+        date_str += String(month);
+
+        date_str += "/";
+
+        if (year < 10)
+          date_str += '0';
+        date_str += String(year);
+      }
+
+      if (gps.time.isValid())
+      {
+        time_str = "";
+        hour = gps.time.hour();
+        minute = gps.time.minute();
+        second = gps.time.second();
+        
+        if (minute > 59)
+        {
+          minute = minute - 60;
+          hour = hour + 1;
+        }
+        hour = (hour + 8);
+        if (hour > 23){
+          hour = hour - 24;
+          date_inc = 1;
+        }else{
+          date_inc = 0;   
+        }
+
+        if (hour < 10)
+          time_str = '0';
+        time_str += String(hour);
+
+        time_str += ":";
+
+        if (minute < 10)
+          time_str += '0';
+        time_str += String(minute);
+
+        time_str += ":";
+
+        if (second < 10)
+          time_str += '0';
+        time_str += String(second);
+      }
+
+    }
+  delay(100);
+}
+void dsm501() {
+//  duration1 = pulseIn(pin_pm10, LOW);
+  duration2 = pulseIn(pin_pm25, LOW);
+//  lowpulseoccupancy1 += duration1;
+  lowpulseoccupancy2 += duration2;
+//  endtime1 = millis();
+  endtime2 = millis();
+//  if ((endtime1-starttime1) > sampletime_ms)
+//  {
+//    ratio1 = (lowpulseoccupancy1-endtime1+starttime1 + sampletime_ms)/(sampletime_ms*10.0);  // Integer percentage 0=>100
+//    concentration1 = 1.1*pow(ratio1,3)-3.8*pow(ratio1,2)+520*ratio1+0.62; // using spec sheet curve
+//      if (ratio1 > 400000){
+//      concentration1 = 0;
+//      ratio1 = 0;
+//    }
+//    lowpulseoccupancy1 = 0;
+//    starttime1 = millis();
+//  }
+    if ((endtime2-starttime2) > sampletime_ms)
+  {
+    ratio2 = (lowpulseoccupancy2-endtime2+starttime2 + sampletime_ms)/(sampletime_ms*10.0);  // Integer percentage 0=>100
+    concentration2 = 1.1*pow(ratio2,3)-3.8*pow(ratio2,2)+520*ratio2+0.62; // using spec sheet curve
+    if (ratio2 > 400000){
+      concentration2 = 0;
+      ratio2 = 0;
+    }
+    lowpulseoccupancy2 = 0;
+    starttime2 = millis();
+  }
+}
+void dht22(){
+    hum = dht.readHumidity();
+    temperature= dht.readTemperature();
+}
+//void mqtt(){
+//  String mqtt_msg;
+//    myFile1 = SD.open("rcvdata.csv", FILE_READ);
+//  while (myFile1.available()) {
+//    mqtt_msg += (char)myFile1.read();
+//  }
+//  myFile1.close();
+//  Serial.println(mqtt_msg.c_str());
+//    client.connect("send", mqtt_username, mqtt_password);
+//    client.publish("tes", "28,6,30.00,81.10,0.00,,,00/00/2000,08:00:00,3256231388,end#27,6,30.00,81.50,0.00,,,00/00/2000,08:00:00,3256231388,end#23,21,nan,nan,0.00,,,00/00/2000,08:00:00,840990861,end#");
+//}
+void mqtt(){
+  String mqtt_msg, rows, carbondioxide,carbonmonoxide,temper,humidi,pm25dsm,latt,longit,daterecord,timerecord,id_esp;
+  int m=0, ct=0, count=0;
+    myFile1 = SD.open("rcvdata.csv", FILE_READ);
+  while (myFile1.available()) {
+    mqtt_msg += (char)myFile1.read();
+  }
+  myFile1.close();
+
+  b:
+  if (mqtt_msg[m] == '#')
+    ct++;
+  m++;
+  if (mqtt_msg[m])
+    goto b;
+  Serial.print("COUNT =");
+  Serial.println(count);
+    Serial.print("CT =");
+  Serial.println(ct);
+  if(client.connect("send", mqtt_username, mqtt_password)){
+    Serial.println("mqtt connect");;
+  }
+  while (count < ct) {
+    rows = getValue(mqtt_msg, '#', count);
+    carbondioxide = getValue(rows, ',', 0);
+    carbonmonoxide = getValue(rows, ',', 1);
+    temper = getValue(rows, ',', 2);
+    humidi = getValue(rows, ',', 3);
+    pm25dsm = getValue(rows, ',', 4);
+    latt = getValue(rows, ',', 5);
+    longit = getValue(rows, ',', 6);
+    daterecord = getValue(rows, ',', 7);
+    timerecord = getValue(rows, ',', 8);
+    id_esp = getValue(rows, ',', 9);
+    Serial.println(String(carbondioxide)); 
+    Serial.println(String(carbonmonoxide));
+    Serial.println(String(temper)); 
+    Serial.println(String(humidi));
+    Serial.println(String(pm25dsm));
+    Serial.println(String(id_esp));
+    count++;
+    client.publish("gateway/temp", temper.c_str());
+    client.publish("gateway/hum", humidi.c_str());
+  } 
+    
+    
+}
